5f89c8cb615dda3feeb640997bd2e430b2a26f28
Your Name <jgbharris@hotmail.co.uk>
2021-04-21 23:53:23 +0100

    code tidied

--- src/TestPartOne/TestPartOne.js
+++ src/TestPartOne/TestPartOne.js
@@ -1,14 +1,14 @@
-function sortArrays(array1, array2) { 
+function sortArrays(array1, array2) {
 
     if (array1.some(isNaN) || array2.some(isNaN)) {
-        console.log("arrays container non-numerical values")
+        console.log("arrays contains non-numerical values")
     } else {
         const combinedArray = array1.concat(array2)
         const sortedArray = combinedArray.sort()
         console.log(sortedArray)
     }
-   
-  }
+
+}
 
 sortArrays([1, 5, "A", 2], [9, 0, 2, 3])
 
