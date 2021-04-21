539c70f09e159d8b034e723f253504c5618c1d80
Your Name <jgbharris@hotmail.co.uk>
2021-04-21 23:31:35 +0100

    adding in test part one

--- src/TestPartOne.js
+++ src/TestPartOne.js
@@ -1,8 +1,15 @@
-function sortArrays(array1, array2) {
-    const combinedArray = array1.concat(array2)
-    console.log(combinedArray)
-    const sortedArray = combinedArray.sort()
-    console.log(sortedArray)
+function sortArrays(array1, array2) { 
+
+    if (array1.some(isNaN) || array2.some(isNaN)) {
+        console.log("arrays container non-numerical values")
+    } else {
+        const combinedArray = array1.concat(array2)
+        const sortedArray = combinedArray.sort()
+        console.log(sortedArray)
+    }
+   
   }
-  
-  sortArrays([1,5,6,2], [9,0,2,3])
+
+sortArrays([1, 5, "A", 2], [9, 0, 2, 3])
+
+sortArrays([1, 5, 8, 2], [9, 0, 2, 3])
